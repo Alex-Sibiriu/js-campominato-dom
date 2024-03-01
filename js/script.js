48b621d460bd34118f75e81719de03caf99e044e
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-03-01 13:39:34 +0100

    [add] Win/Lose Condition

--- js/script.js
+++ js/script.js
@@ -1,13 +1,22 @@
+/**************
+  ELEMENTS
+**************/
 const grid = document.getElementById('grid-container');
 const btnStart = document.getElementById('btn-start');
 const btnRestart = document.getElementById('btn-restart');
 const difficultyArray = [49, 81, 100];
+const bombArray = [];
+const bombNumber = 16;
 let maxSquares;
-
-btnStart.addEventListener('click', startGame);
-btnRestart.addEventListener('click', startGame);
+let win;
 
 grid.append(btnStart);
+
+/**************
+  EVENTS
+**************/
+btnStart.addEventListener('click', startGame);
+btnRestart.addEventListener('click', startGame);
 
 /**************
   FUNCTIONS
@@ -19,6 +28,7 @@ function startGame() {
   const difficulty = document.getElementById('difficulty').value;
 
   setMaxSquares(difficulty);
+  createBombs();
   createGrid(maxSquares);
 
   btnStart.classList.add('d-none');
@@ -28,6 +38,17 @@ function startGame() {
 // Funzione per impostare il numero di quadrati
 function setMaxSquares(difficultyValue) {
   maxSquares = difficultyArray[difficultyValue];
+}
+
+// Funzione per creare le bombe
+function createBombs() {
+  do {
+    const bombID = Math.ceil(Math.random() * maxSquares);
+    if (!(bombArray.includes(bombID))) {
+      bombArray.push(bombID);
+    }
+  } while (bombArray.length < bombNumber);
+  console.log(bombArray);
 }
 
 // Funzione per creare la griglia
@@ -44,9 +65,11 @@ function createSquare(indexN) {
   const cell = document.createElement('div');
   cell.className = 'square';
 
+  cell._cellID = indexN;
+
   cell.addEventListener('click', function() {
-    this.classList.toggle('clicked');
-    this.innerHTML = (this.classList.contains('clicked')) ? indexN : '';
+    this.classList.add('empty');
+    checkLose(cell);
     checkWin();
   })
 
@@ -55,21 +78,42 @@ function createSquare(indexN) {
 
 // Funzione per controllare se il giocatore ha vinto
 function checkWin() {
-  const allClickedSquares = document.querySelectorAll('.clicked');
+  const allClickedSquares = document.querySelectorAll('.empty');
 
-  if (allClickedSquares.length === maxSquares) printResult();
+  if (allClickedSquares.length === (maxSquares - bombNumber)) {
+    win = true;
+    printResult(win);
+  } 
+}
+
+// Funzione per vedere se hai cliccato una bomba
+function checkLose(cell) {
+  if (bombArray.includes(cell._cellID)) {
+    cell.classList.add('bomb');
+    win = false;
+    printResult(win);
+  }
 }
 
 // Funzione per stampare il risultato
-function printResult() {
+function printResult(win) {
+  if (win) {
   const message = document.createElement('div');
   message.className = 'game-end';
   message.classList.add('win');
   message.innerHTML = 'Hai Vinto!';
   grid.append(message);
+  } else {
+  const message = document.createElement('div');
+  message.className = 'game-end';
+  message.classList.add('lose');
+  message.innerHTML = 'Hai Perso!';
+  grid.append(message);
+  }
 }
 
 // Funzione di reset della griglia
 function reset() {
   grid.innerHTML = '';
+  bombArray.splice(0);
 }
