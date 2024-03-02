a9279f243dbccf7bcf542aad7b1c27339f125da2
Alex-Sibiriu <alexsibiriu@protonmail.com>
2024-03-02 15:40:35 +0100

    [add] Check bomb around

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@
 const grid = document.getElementById('grid-container');
 const btnStart = document.getElementById('btn-start');
 const btnRestart = document.getElementById('btn-restart');
+const showScore = document.getElementById('score-number');
+
 const difficultyArray = [49, 81, 100];
 const bombArray = [];
 const bombNumber = 16;
@@ -18,7 +20,6 @@ grid.append(btnStart);
 **************/
 btnStart.addEventListener('click', startGame);
 btnRestart.addEventListener('click', startGame);
-console.log(bombArray)
 
 /**************
   FUNCTIONS
@@ -69,16 +70,27 @@ function createSquare(indexN) {
   cell._cellID = indexN;
 
   cell.addEventListener('click', function() {
-    if (!(this.classList.contains('empty'))) {
-      score++;
-    }
-    this.classList.add('empty');
-    checkLose(this);
-    console.log(score)
-    checkWin();
+
+    leftClick(this);
+    // cell.innerHTML = checkAround(cell);
+    // this.classList.add('empty');
+    // score++;
+    // checkLose(this);
+    // showScore.innerHTML = score + '/' + (maxSquares - bombNumber);
+    // checkWin();
   })
 
   return cell;
+}
+
+// Funzione del click sinistro
+function leftClick(cell) {
+  cell.innerHTML = checkAround(cell);
+  cell.classList.add('empty');
+  score++;
+  checkLose(cell);
+  showScore.innerHTML = score + '/' + (maxSquares - bombNumber);
+  checkWin();
 }
 
 // Funzione per controllare se il giocatore ha vinto
@@ -95,7 +107,7 @@ function checkLose(cell) {
     cell.classList.add('bomb');
     win = false;
     score--;
-    showBombs()
+    showBombs();
     printResult(win);
   }
 }
@@ -139,9 +151,37 @@ function printResult(win) {
   grid.append(message);
 }
 
+// Funzione per guardare se le celle vicine sono bombe
+function checkAround(cell) {
+  const gridWidth = Math.sqrt(maxSquares);
+  const isLeftEdge = (cell._cellID % gridWidth === 1);
+  const isRightEdge = (cell._cellID % gridWidth === 0);
+  const isFirstRow = cell._cellID <= gridWidth;
+  const isLastRow = cell._cellID > maxSquares - gridWidth;
+  const allDirections = [- gridWidth - 1, - gridWidth, - gridWidth + 1, - 1, + 1, + gridWidth - 1, + gridWidth, + gridWidth + 1];
+  let bombsAround = 0;
+  
+  for (let i = 0; i < allDirections.length; i++) {
+    const direction = allDirections[i];
+    const nearCell = cell._cellID + direction;
+
+    if (
+        !(isLeftEdge && (direction === - gridWidth - 1 || direction === - 1 || direction === + gridWidth - 1)) &&
+        !(isRightEdge && (direction === - gridWidth + 1 || direction === + 1 || direction === + gridWidth + 1)) &&
+        !(isFirstRow && (direction === - gridWidth + 1 || direction === - gridWidth || direction === - gridWidth - 1)) &&
+        !(isLastRow && (direction === + gridWidth + 1 || direction === + gridWidth || direction === + gridWidth - 1))
+      ) {
+    if (bombArray.includes(nearCell)) bombsAround++;
+    }
+  }
+
+  return bombsAround;
+}
+
 // Funzione di reset della griglia
 function reset() {
   grid.innerHTML = '';
   bombArray.splice(0);
   score = 0;
+  showScore.innerHTML = score;
 }
